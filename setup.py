654f13887fc3cf4cfdca4d8fd17e2522acf3ddec
Ankit vishwa <aviatankit@gmail.com>
2025-11-19 14:58:36 +0530

    Update author name in setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     name="port-intent-detector",
     version="0.1.0",
     description="A small tool that monitors outbound connections and infers intent based on port",
-    author="Your Name / Mkali10",
+    author="Mkali10",
     url="https://github.com/Mkali10/port_intent_detector",
     packages=find_packages(),
     install_requires=[
